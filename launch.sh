5c79dd538ec0109dff1dea076c0bfb88da8938e7
Joanthan17 <72089812+Joanthan17@users.noreply.github.com>
2021-09-16 20:14:17 +0300

    launch firefox kiosk mode with notion built in

---
+++ launch.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+#firefox -new-window https://notion.so -P notion-profile -no-remote --kiosk --class=notion
+firefox -new-window https://notion.so -P notion-profile -no-remote --class=notion
